70e143f857f60522b65be0719a6dfb51a757c4e4
alex <kurochkinalexei@yandex.ru>
2023-05-29 21:29:50 +0300

    -

--- library/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import *
-
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['name', 'author', 'publisher', 'image', 'description', 'price', 'is_not_visible']
